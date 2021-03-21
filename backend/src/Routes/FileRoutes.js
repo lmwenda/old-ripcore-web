d3f5d2422c2174985e78e4cb12b2d32c884f7570
dovakiin0 <subedibs@outlook.com>
2021-03-21 20:03:09 +0545

    download fix

--- backend/src/Routes/FileRoutes.js
+++ backend/src/Routes/FileRoutes.js
@@ -40,7 +40,7 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/download/:pack", async (req, res) => {
-  const file = File.findOne({ name: req.params.pack });
+  const file = await File.findOne({ name: req.params.pack });
   if (!file) return res.status(404).send("No such packs!");
   res.download(file.url, (err) => {
     if (err) return res.status(500).send("Error Downloading File!");
